6ae73c0c3db9d1737e5792f40d332657031df15d
Sridhar Prasad Panneerselvam <panneerselvam.s@husky.neu.edu>
2020-05-27 02:41:31 -0400

    Assignment2 Webapp-UI front end Angular base commit

---
+++ src/app/interface/IResponse.ts
@@ -0,0 +1,18 @@
+export interface IResponse{
+    token: any;
+    user: Iuser;
+}
+
+export interface Iuser{
+    firstName: String;
+    lastName: String;
+    userName : String;
+    userPassword: String;
+}
+
+
+// export interface userResponse{
+//     userEmail: String;
+//     userPassword: String;
+// }
+
