8f9c06db216cd71c44329f343b432f0f114f7a20
Sridhar Prasad Panneerselvam <panneerselvam.s@husky.neu.edu>
2020-05-28 14:46:44 -0400

    frontend final commit for testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LayoutModule } from './layout/layout.module';
 import { AuthService } from 'src/auth-guard';
+import { LoginguardService } from 'src/login-guard';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent},
+  {path: 'login', component: LoginComponent,canActivate:[LoginguardService]},
   {path: 'layout', loadChildren: () => LayoutModule, canActivate: [AuthService]},
   {path: 'signUp', component: LoginComponent},
   {path: '**', redirectTo: 'login'}
